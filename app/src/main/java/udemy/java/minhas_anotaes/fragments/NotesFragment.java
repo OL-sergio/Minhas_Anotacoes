1d18bb56f435b17c227b7b2315f488ad60c4ab97
Sérgio <a18074@alunos.ipca.pt>
2022-10-03 18:34:19 +0100

    Initial commit

---
+++ app/src/main/java/udemy/java/minhas_anotaes/fragments/NotesFragment.java
@@ -0,0 +1,41 @@
+package udemy.java.minhas_anotaes.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import udemy.java.minhas_anotaes.databinding.FragmentNotesBinding;
+
+public class NotesFragment extends Fragment {
+
+    private FragmentNotesBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentNotesBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
